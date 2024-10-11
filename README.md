389d5d675945e7f97395c838c2324b56c273006b
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2024-10-10 22:15:42 -0300

    Adiciona estado da lista de peças

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # kanoodle-vanilla-inova
 
 - [ ] Listagem de peças
+    - [x] Estado da lista de peças
+    - [ ] Desenhar as peças
 - [ ] Capacidade de selecionar uma peça
 - [ ] Girar a peça e espelhar
 - [ ] Colocar uma peça no tabuleiro
+    - [x] Estado do tabuleiro
+    - [x] Desenhar o tabuleiro
 - [ ] Remover uma peça do tabuleiro
--- main.js
+++ main.js
@@ -1,28 +1,61 @@
+//Cria estado do tabuleiro
+
 const tabuleiro = [];
 
-for(let linha = 0; linha < 5; linha++){
-    tabuleiro[linha] = [];
-    for(let coluna = 0; coluna < 11; coluna++){
-        tabuleiro[linha][coluna] = " ";
-    }
+for (let linha = 0; linha < 5; linha++) {
+  tabuleiro[linha] = [];
+  for (let coluna = 0; coluna < 11; coluna++) {
+    tabuleiro[linha][coluna] = " ";
+  }
 }
 
 tabuleiro[1][3] = "a";
 console.log(tabuleiro);
 document.body.appendChild(imprimeTabuleiro(tabuleiro));
 
-function imprimeTabuleiro(tabuleiro){
-    const item = document.createElement("div");
-    item.classList.add("tabuleiro");
+// Cria lista de peças
+const pecas = {
+  a: {
+    disponivel: true,
+    tipo: "a",
+    formato: [[0, 0]],
+  },
+  b: {
+    disponivel: true,
+    tipo: "b",
+    formato: [[0, 0],[0, 1]],
+  },
+};
 
-    for(let linha = 0; linha < 5; linha++){
-        for(let coluna = 0; coluna < 11; coluna++){
-            const div = document.createElement("div");
-            div.classList.add("celula");
-            div.textContent = tabuleiro[linha][coluna];
-            item.appendChild(div);
-        }
+document.body.appendChild(imprimePecas(pecas));
+
+function imprimePecas(listaPecas) {
+  const pecas = document.createElement("ol");
+  pecas.classList.add("pecas")
+  for(let chave in listaPecas){
+    console.log(chave);
+    const peca = document.createElement("li");
+    peca.classList.add("peca");
+    peca.textContent = listaPecas[chave].tipo;
+    pecas.appendChild(peca);
+  }
+  
+
+  return pecas;
+}
+
+function imprimeTabuleiro(tabuleiro) {
+  const item = document.createElement("div");
+  item.classList.add("tabuleiro");
+
+  for (let linha = 0; linha < 5; linha++) {
+    for (let coluna = 0; coluna < 11; coluna++) {
+      const div = document.createElement("div");
+      div.classList.add("celula");
+      div.textContent = tabuleiro[linha][coluna];
+      item.appendChild(div);
     }
+  }
 
-    return item;
+  return item;
 }
