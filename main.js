c1a2532f2eec53e714cc174f611fd453cfa49792
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2024-10-10 21:49:42 -0300

    Adiciona representação do tabuleiro

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+const tabuleiro = [];
+
+for(let linha = 0; linha < 5; linha++){
+    tabuleiro[linha] = [];
+    for(let coluna = 0; coluna < 11; coluna++){
+        tabuleiro[linha][coluna] = " ";
+    }
+}
+
+tabuleiro[1][3] = "a";
+console.log(tabuleiro);
+document.body.appendChild(imprimeTabuleiro(tabuleiro));
+
+function imprimeTabuleiro(tabuleiro){
+    const item = document.createElement("div");
+    item.classList.add("tabuleiro");
+
+    for(let linha = 0; linha < 5; linha++){
+        for(let coluna = 0; coluna < 11; coluna++){
+            const div = document.createElement("div");
+            div.classList.add("celula");
+            div.textContent = tabuleiro[linha][coluna];
+            item.appendChild(div);
+        }
+    }
+
+    return item;
+}
